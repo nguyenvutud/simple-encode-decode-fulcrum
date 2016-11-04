9d7ad89c5cdbbf3885967939153d2559a4138403
Vu Nguyen <vu.nguyen@tu-dresden.de>
2016-11-04 13:43:28 +0100

    This is the temporary version

--- src/fulcrum/fulcrum.cpp
+++ src/fulcrum/fulcrum.cpp
@@ -22,7 +22,10 @@
 #include <kodo_fulcrum/fulcrum_codes.hpp>
 
 using namespace std;
-//This is the first version of project
+//This is a temporary change
+//everything changes with store in here
+
+
 int run(float e1,float e2, uint32_t expansion);
 float min(float a, float b);
 ofstream myfile;
